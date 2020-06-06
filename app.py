c771c65a8798c5f630961485ff092b0ce54e12ba
miguel <miguel.junior.mat@hotmail.com>
2020-06-06 18:38:39 -0300

    update

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def updateTodo(item_id):
 
 if __name__ == '__main__':
   port = int(os.environ.get("PORT", 5000))
-  app.run(host='0.0.0.0', port=port,debug=True)
+  app.run(host='0.0.0.0', port=port)
