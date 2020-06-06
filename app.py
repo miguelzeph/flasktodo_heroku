ea62e60ae170487ccd8d53025fa74b65446eba5f
miguel <miguel.junior.mat@hotmail.com>
2020-06-06 14:56:12 -0300

    enviar heroku

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template, request, jsonify
 from pusher import Pusher
 import json
+import os
 
 # create flask app
 app = Flask(__name__)
@@ -47,4 +48,9 @@ def updateTodo(item_id):
   return jsonify(data)
 
 # run Flask app in debug mode
-app.run(debug=True)
+#app.run(debug=True)
+
+
+if __name__ == '__main__':
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port,debug=True)
